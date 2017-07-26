830bf1b0bd96ef400428e953ba583ebbd620b6ca
Daes <Daes>
2017-07-26 17:35:45 -0600

    Final Comp test

--- Comp/getStates.py
+++ Comp/getStates.py
@@ -1,53 +1,78 @@
+##Laboratorio #2 Dise;o de Lenguajes de Programacion
+##Diego Javier Alvarez 14104
+
+
 def getStates(Automata):
-	states = {}
-	ctr = 0
-	for currentc in Automata(0,len()):
-		##Si es parentesis, hacer una cosa, si es or o kleene, hacer otra, de lo contrario, seguir adelante.
-		##Caso en el que es PARENTESIS
-		if Automata[currentc] == "(":
-			#funcion parentesis
+        states = []
+        currentstate = 0
+        for x in range(0, (len(Automata)-1)):
+                print (x)
+                if (Automata[x] not in {")", "*"}):
+                        if(Automata[x] == "(") :
+                                x= x+1
+                                parenthesend = False
+                                while (parenthesend == False):
+                                        if (Automata[x] != ")" ):
+                                                x = x + 1
+                                        else:
+                                                parenthesend = True
+                        elif(Automata[x+1] != "|" ):
+                                states.append(currentstate)
+                                currentstate=currentstate+1
+                        else:
+                                x=x+1
+                                print("Ignoring value " + str(Automata[x]))
 
-		##Caso en el que es OR o KLEENE	
-		##Si el siguiente es un or, hacer algo, si es kleene, hacer otra cosa.
-		elif (Automata[currentc +1] == "*" or Automata[currentc + 1] == "|"):
-			
-			#Verificar si esta kleeneado o es parte de un OR
-			##Caso en el que es un KLEENE
-			if Automata[currentc+1] == "*":
-				#Si es kleeneado, tomarlo como un estado y saltar una posicion.
-				states[a] = a
-				ctr = ctr+1
-				currentc = currentc+2
+                                
+                else:
+                        print("Ignoring value " + str(Automata[x]))
+                    
 
-			##Caso en el que es un OR
-			elif Automata[currentc +1] == "|":
-				
-				#Si es parte de un OR, tomarlo como un estado y saltar dos posiciones (excepto si el siguiente es un parentesis o un or o kleene)
-				##Caso en el que es un PARENTESIS
-				if Automata[currentc+1] == "(":
-					#si el siguiente caracer es un parentesis, hacer lo que se debe hacer con parentesis
-					#Funcion parentesis
-				
-				##Caso en el que es un OR o un KLEENE (error)
-				elif (Automata[currentc+1] == "*" or Automata[currentc+1] "|"):
-					#No tendria sentido que el siguiente sea otro OR o Kleene, entonces ignoramos eso.
-					print("Ignoring current character (at pos:" + currentc + "), since it's not valid.")
-					#Esto significa que hay un error de redaccion y se ignora.
-				
-				##Caso en el que es un Caracter Valido
-				else: 
-					state[ctr] = ctr
-					ctr = ctr +1
-					currentc = currentc+2
+        return(states)
+
+def getSymbols(Automata):
+        symbols = []
+        for x in range(0, (len(Automata)-1)):
+                if (Automata[x] not in {"*", "|", ")", "("}) and (Automata[x] not in symbols):
+                        symbols.append(Automata[x])
+                                         
+                else:
+                        pass
+        return(symbols)
 
 
-		else:
-			states[ctr] = ctr
-			ctr = ctr+1
-			currentc = currentc+1
-	return states
+def getStart(Automata):
+        x = 0
+        currentstate = 0
+        slist = []
+        end =False
+        print (Automata[x:])
+        while end == False:
+                if (Automata[x+1] != "*"):
+                        slist.append(currentstate)
+                        currentstate = currentstate + 1
+                        end = True
+                else:
+                        x= x+2
+                        slist.append(currentstate)
+                        currentstate = currentstate + 1
+        return(slist)
+                
+                
+        
 
 
+
+
+
+Input = input("Ingrese una cadena usando cualquier simbolo. Los Operadores son: | (or), * (kleene) y se aceptan parentesis. Aun no hay funcionalidad para epsilon: ")
+
+sts = getStates(Input)
+syms = getSymbols(Input)
+start= getStart(Input)
+print ("States = {" + str(sts) + "}")
+print ("Symbols = {" + str(syms) + "}")
+print ("Start = {" + str(start) + "}")
 
 
 
---
+++ Comp/lab2.py
@@ -0,0 +1,78 @@
+##Laboratorio #2 Dise;o de Lenguajes de Programacion
+##Diego Javier Alvarez 14104
+
+
+def getStates(Automata):
+        states = []
+        currentstate = 0
+        for x in range(0, (len(Automata)-1)):
+                print (x)
+                if (Automata[x] not in {")", "*"}):
+                        if(Automata[x] == "(") :
+                                x= x+1
+                                parenthesend = False
+                                while (parenthesend == False):
+                                        if (Automata[x] != ")" ):
+                                                x = x + 1
+                                        else:
+                                                parenthesend = True
+                        elif(Automata[x+1] != "|" ):
+                                states.append(currentstate)
+                                currentstate=currentstate+1
+                        else:
+                                x=x+1
+                                print("Ignoring value " + str(Automata[x]))
+
+                                
+                else:
+                        print("Ignoring value " + str(Automata[x]))
+                    
+
+        return(states)
+
+def getSymbols(Automata):
+        symbols = []
+        for x in range(0, (len(Automata)-1)):
+                if (Automata[x] not in {"*", "|", ")", "("}) and (Automata[x] not in symbols):
+                        symbols.append(Automata[x])
+                                         
+                else:
+                        pass
+        return(symbols)
+
+
+def getStart(Automata):
+        x = 0
+        currentstate = 0
+        slist = []
+        end =False
+        print (Automata[x:])
+        while end == False:
+                if (Automata[x+1] != "*"):
+                        slist.append(currentstate)
+                        currentstate = currentstate + 1
+                        end = True
+                else:
+                        x= x+2
+                        slist.append(currentstate)
+                        currentstate = currentstate + 1
+        return(slist)
+                
+                
+        
+
+
+
+
+
+Input = input("Ingrese una cadena usando cualquier simbolo. Los Operadores son: | (or), * (kleene) y se aceptan parentesis. Aun no hay funcionalidad para epsilon, los espacios cuentan como simbolos tambien!S: ")
+
+sts = getStates(Input)
+syms = getSymbols(Input)
+start= getStart(Input)
+print ("States = {" + str(sts) + "}")
+print ("Symbols = {" + str(syms) + "}")
+print ("Start = {" + str(start) + "}")
+
+
+
